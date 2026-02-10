fbbf56d73f5dae8c6682f47dc021e7e64386dcf5
mikeAbril <jaimecarreno734@gmail.com>
2026-02-09 21:33:09 -0500

    esperanza gomez

--- models/usuariosModel.js
+++ models/usuariosModel.js
@@ -5,7 +5,9 @@ const usuario = new mongoose.Schema({
     edad:{type:Number},
     fechanacimiento:{type:Date, default:Date.now},
     email:{type:String,unique:true, required: true},
-    estado:{type:Number,default:1}//0 inactivo   1 activo
+    estado:{type:Number,default:1},
+    password:{type: String, required: true, minlenght:8, maxlenght:20},
+    rol: {type:String, emun:["admin","usuario"], default:"usuario"} 
 });
 
 export default mongoose.model("Usuario",usuario)
