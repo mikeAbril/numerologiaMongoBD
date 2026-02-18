9bfbbdd22d46fda8260bde9ad8540a8e26eb21c3
mikeAbril <jaimecarreno734@gmail.com>
2026-02-18 16:35:50 -0500

    cambiando database de localhost a mongoatlas

--- database/cnx-mongo.js
+++ database/cnx-mongo.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const conectarMongo = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/numerologia");
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("✅ Mongo conectado");
   } catch (error) {
     console.error("❌ Error Mongo:", error);
