4c01ede7016937b97bf67e998f291fba2223d1dc
deivi18contreras <deivibogoya@gmail.com>
2026-03-09 23:23:46 -0500

    pruebas

--- backend/routes/loginRouter.js
+++ backend/routes/loginRouter.js
@@ -5,7 +5,7 @@ import { validarCampos } from "../middlewares/validarCampos.js";
 import { forgotPassword, resetPassword } from "../controllers/usuariosController.js";
 const router = Router();
 
-router.post("/",
+router.post("/login",
     [
         check("email", "El fomato del correo es inválido").isEmail().notEmpty(),
         check("password", "La contraseña es obligatoria").notEmpty(),
