83f03f091ca0dd163eb2a01fbaee8f28ed01b3dd
deivi18contreras <deivibogoya@gmail.com>
2026-03-11 18:51:20 -0500

    Pruebas

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(cors({
   ],
   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
   allowedHeaders: ["Content-Type","Authorization","x-token"]
-}));
+}));-
 conectarMongo()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
